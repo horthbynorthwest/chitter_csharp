fe1734ad12ada92a03f9e852abda50463b1fecd9
Georgie Horth <georgiehorth@gmail.com>
2021-02-12 18:44:45 +0000

    Api steps sorted

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -19,15 +19,13 @@ namespace API.Controllers
 
         [HttpGet]
         [AllowAnonymous]
-
         public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
         {
             return await _context.Users.ToListAsync();
         }
 
-        [Authorize]
+        // [Authorize]
         [HttpGet("{id}")]
-
         public async Task<ActionResult<AppUser>> GetUsers(int id)
         {
             return await _context.Users.FindAsync(id);
